84beea5f914f46057b4429723a8c9359598efc84
al3xk0s <al3xk0s@yandex.ru>
2023-01-03 15:41:36 +0600

    Добавил try turtle do handler

---
+++ src/TryTurtleDoHandler.ts
@@ -0,0 +1,36 @@
+interface ITryTurtleDoHandler {
+    exec() : boolean;
+
+    get currentAttemptCount(): number;
+}
+
+class TryTurtleDoHandler implements ITryTurtleDoHandler {
+    constructor(
+        private readonly executor: () => boolean, 
+        private readonly handler: () => void,
+        public readonly attemptLimit: number,
+    ) { }
+
+    private _currentAttemptCount: number = 0;
+
+    exec(): boolean {
+        this._currentAttemptCount = 0;
+        
+        let isSuccess = this.executor();
+
+        while(!isSuccess) {
+            this.handler();
+            isSuccess = this.executor();
+            this._currentAttemptCount++;
+
+            if(this._currentAttemptCount >= this.attemptLimit) break;
+        }
+
+        return isSuccess;
+    }
+
+    get currentAttemptCount(): number {
+        return this._currentAttemptCount;
+    }
+}
+
